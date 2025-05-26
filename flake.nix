de32596cf028a5cce8d90874fcd8430a46b76bbb
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-05-26 16:12:34 -0500

    init flake.nix

--- flake.nix
+++ flake.nix
@@ -1,1 +1,28 @@
+{
+  description = "A NixOS configuration for sysmind.nix";
+  outputs = { self, nixpkgs, home_manager, nixos_hardware }@inputs:
+    let
+      import_modules = import ./resources/nix/import_all.nix;
+      base_modules = import_modules ./system ++ home_manager.nixosModules.home-manager;
 
+      make_framework_16 = nixpkgs.lib.nixosSystem {
+        modules = base_modules ++ [ nixos_hardware.nixosModules.framework-16-7040-amd ];
+        specialArgs = { inherit inputs; };
+      };
+    in
+    {
+      nixosConfigurations = { };
+    };
+  inputs = {
+    home_manager = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager";
+    };
+    nixos_hardware = {
+      url = "github:NixOS/nixos-hardware/master";
+    };
+    nixpkgs = {
+      url = "github:nixos/nixpkgs?ref=25.05";
+    };
+  };
+}
