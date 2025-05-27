779f030d17290c97e976e5f8030c3edac79b5911
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-05-27 13:02:33 -0500

    fix

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
   inputs = {
     home_manager = {
       inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:nix-community/home-manager?ref=25.05";
+      url = "github:nix-community/home-manager?ref=release-25.05";
     };
     nixos_hardware = {
       url = "github:NixOS/nixos-hardware/master";
