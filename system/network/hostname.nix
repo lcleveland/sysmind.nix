561b8a83b969c1c2661508fe322c433b93c9115f
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-05-29 17:16:53 -0500

    refactor network

--- system/network/hostname.nix
+++
@@ -1,9 +0,0 @@
-{ lib, ... }: {
-  options.system.network.hostname = lib.mkOption {
-    type = lib.types.str;
-    default = "sysmind";
-    description = ''
-      The hostname of the system.
-    '';
-  };
-}
--- system/network/network.nix
+++ system/network/network.nix
@@ -1,5 +1,19 @@
-{ config, ... }: {
+{ config, ... }:
+let
+  network = config.system.network;
+in
+{
+  options.system.network = {
+    hostname = lib.mkOption {
+      type = lib.types.str;
+      default = "sysmind";
+      description = ''
+        The hostname of the system.
+      '';
+    };
+  };
+
   config = {
-    networking.hostName = config.system.network.hostname;
+    networking.hostName = network.hostname;
   };
 }
