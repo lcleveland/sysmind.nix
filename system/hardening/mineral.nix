a1eeda064d52a456e0d37178aa31647d572d2ae9
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-05-29 12:09:14 -0500

    test

--- system/hardening/mineral.nix
+++ system/hardening/mineral.nix
@@ -1,16 +1,14 @@
 { config, inputs, lib, pkgs, ... }:
 {
   imports = [ "${inputs.nix_mineral}/nix-mineral.nix" ];
-  config = {
-    nm-overrides = {
-      desktop = {
-        home-exec.enable = true;
-        var-lib-exec.enable = true;
-        usbguard-disable.enable = true;
-      };
-      security = {
-        disable-intelme-kmodules.enable = true;
-      };
+  nm-overrides = {
+    desktop = {
+      home-exec.enable = true;
+      var-lib-exec.enable = true;
+      usbguard-disable.enable = true;
+    };
+    security = {
+      disable-intelme-kmodules.enable = true;
     };
   };
 }
