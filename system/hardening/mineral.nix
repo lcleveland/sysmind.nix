a5d006fe0c755baebda3682e71686ff64c6efb87
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-05-29 12:27:22 -0500

    test

--- system/hardening/mineral.nix
+++ system/hardening/mineral.nix
@@ -4,7 +4,7 @@
     "${inputs.nix_mineral}/nix-mineral.nix"
   ];
   config = {
-    config.nix-mineral.overrides = {
+    nix_mineral.nix-mineral.overrides = {
       desktop = {
         home-exec.enable = true;
         var-lib-exec.enable = true;
