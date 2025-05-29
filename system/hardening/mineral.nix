c2be843c1865aaa8b0695fc2d0aca5ea9d963dc6
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-05-29 12:07:25 -0500

    add some sane overrides

--- system/hardening/mineral.nix
+++ system/hardening/mineral.nix
@@ -1,4 +1,16 @@
 { config, inputs, lib, pkgs, ... }:
 {
   imports = [ "${inputs.nix_mineral}/nix-mineral.nix" ];
+  config = {
+    config.nm-overrides = {
+      desktop = {
+        home-exec.enable = true;
+        var-lib-exec.enable = true;
+        usbguard-disable.enable = true;
+      };
+      security = {
+        disable-intelme-kmodules.enable = true;
+      };
+    };
+  };
 }
