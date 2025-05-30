06aa39f4e96de73a951e90adc85dd460a1b2b7e6
Lyle Cleveland <7327593+lcleveland@users.noreply.github.com>
2025-05-29 19:21:06 -0500

    test

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     let
       inputs = base_inputs // sysmind_users.inputs;
       import_modules = import ./resources/nix/import_modules.nix;
-      base_modules = import_modules ./system ++ [ home_manager.nixosModules.home-manager /etc/nixos/hardware-configuration.nix ] ++ sysmind_users.nixosModules.default;
+      base_modules = import_modules ./system ++ [ home_manager.nixosModules.home-manager niri.nixosModules.niri /etc/nixos/hardware-configuration.nix ] ++ sysmind_users.nixosModules.default;
       make_framework_16 = nixpkgs.lib.nixosSystem {
         modules = base_modules ++ [ nixos_hardware.nixosModules.framework-16-7040-amd ];
         specialArgs = { inherit inputs; };
